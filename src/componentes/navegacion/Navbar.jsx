4c5ffcc95306b24a7fb79f293590d94e112a1f04
consentir.fabri <brenda.afabri@gmail.com>
2022-09-30 22:27:04 -0300

    cart

--- src/componentes/navegacion/Navbar.jsx
+++ src/componentes/navegacion/Navbar.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { CartWidget } from "../CartWidget/CartWidget";
+import  CartWidget  from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
 import {CATEGORY_RECARGAS, CATEGORY_CERAMICA} from "../../constants"
 
